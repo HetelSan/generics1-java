216f5a8441a6f61a5e361ebf69f48d3c9d02d5b5
Hetel Santos <hetel.santos@gmail.com>
2019-05-08 14:24:13 -0300

    List of Object

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,7 @@ package application;
 
 import java.util.Scanner;
 
-import services.PrintServiceString;
+import services.PrintService;
 
 public class Program {
 
@@ -10,18 +10,21 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		PrintServiceString ps = new PrintServiceString();
+		PrintService ps = new PrintService();
 		
 		System.out.println("How many values? ");
 		int n = sc.nextInt();
 		
+		ps.addValue("Maria");
+		
 		for (int i = 0; i < n; i++) {
-			String value = sc.next();
+			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
 		
 		ps.print();
-		System.out.println("First: " + ps.first());
+		Integer x = (Integer) ps.first();
+		System.out.println("First: " + x);
 		sc.close();
 
 	}
